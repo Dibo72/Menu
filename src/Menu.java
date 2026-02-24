153b1e999caeec2c494cc5a76e4ef2fa091b3581
Dibo72 <humanida133@gmail.com>
2026-02-24 12:34:53 +0100

    Ejercicio acabado

--- src/Menu.java
+++ src/Menu.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class Menu {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        boolean seguir = true;
+        do {
+            System.out.println("1=numeros del 1 al 10, 2=pares del 1 al 20, 3=salir");
+            int opcion = new Scanner(System.in).nextInt();
+            switch (opcion) {
+                case 1:
+                    for (int i = 1; i <= 10; i++) {
+                        System.out.println(i);
+                    }
+                    break;
+                case 2:
+                    for (int i = 1; i <= 20; i++) {
+                        if (i%2 == 0){
+                            System.out.println(i);
+                        }
+                    }
+                    break;
+                case 3:
+                    seguir = false;
+                    break;
+            }
+        }while(seguir);
     }
 }
